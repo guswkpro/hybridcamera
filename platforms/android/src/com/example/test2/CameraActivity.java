a27b4a77cf97551c79dbf90331d78060e0893b00
unknown <>
2016-02-15 05:50:48 +0900

    Aaa

--- platforms/android/src/com/example/test2/CameraActivity.java
+++ platforms/android/src/com/example/test2/CameraActivity.java
@@ -20,9 +20,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
